13fe7546d52564f891f5183f5c1492cb315637b5
Jonny Burger <jonathanburger11@gmail.com>
2023-05-04 10:51:26 +0200

    get it running

--- src/Player.tsx
+++ src/Player.tsx
@@ -1,6 +1,7 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useMemo } from 'react';
 import { View } from 'react-native';
 import type { LooseComponentType } from './core';
+import { Internals, TimelineContextValue } from 'remotion';
 
 type Props<T> = {
   component: LooseComponentType<T>;
@@ -11,9 +12,24 @@ export function Player<T extends JSX.IntrinsicAttributes>({
   component: Comp,
   inputProps,
 }: PropsWithChildren<Props<T>>) {
+  const timelineContext: TimelineContextValue = useMemo(() => {
+    return {
+      audioAndVideoTags: { current: [] },
+      frame: 0,
+      imperativePlaying: { current: false },
+      isPlaying: false,
+      playbackRate: 1,
+      playing: false,
+      rootId: '0',
+      setPlaybackRate: () => {},
+    };
+  }, []);
+
   return (
     <View>
-      <Comp {...inputProps} />
+      <Internals.Timeline.TimelineContext.Provider value={timelineContext}>
+        <Comp {...inputProps} />
+      </Internals.Timeline.TimelineContext.Provider>
     </View>
   );
 }
