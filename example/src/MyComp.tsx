75557206fce82b6151b28d57664ee7e58d6d0b52
Jonny Burger <jonathanburger11@gmail.com>
2023-05-07 16:10:17 +0200

    chore: ok

--- example/src/MyComp.tsx
+++ example/src/MyComp.tsx
@@ -1,58 +1,27 @@
 import React, { useMemo } from 'react';
-import { StyleSheet, Text, View, ViewStyle } from 'react-native';
-import {
-  useCurrentFrame,
-  Sequence,
-  useVideoConfig,
-  interpolateColors,
-} from 'remotion';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',
-  },
-  inner: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+import { View, ViewStyle } from 'react-native';
+import Svg, { Path } from 'react-native-svg';
+import { Sequence, useVideoConfig } from 'remotion';
 
 export const MyComp: React.FC = () => {
-  const frame = useCurrentFrame();
-  const { durationInFrames } = useVideoConfig();
-
   const style: ViewStyle = useMemo(() => {
     return {
       flex: 1,
-      backgroundColor: interpolateColors(
-        frame,
-        [0, durationInFrames],
-        ['orange', 'green']
-      ),
+      backgroundColor: '#0133CC',
       justifyContent: 'center',
       alignItems: 'center',
     };
-  }, [durationInFrames, frame]);
+  }, []);
+
+  const { height, width } = useVideoConfig();
 
   return (
     <View style={style}>
       <Sequence layout="none" from={10} durationInFrames={Infinity}>
-        <Inner />
+        <Svg viewBox={`0 0 ${width} ${height}`}>
+          <Path d={`M 0 0 L 100 100`} stroke="white" strokeWidth={10} />
+        </Svg>
       </Sequence>
-    </View>
-  );
-};
-
-export const Inner: React.FC = () => {
-  const frame = useCurrentFrame();
-
-  return (
-    <View style={styles.inner}>
-      <Text>{frame}</Text>
     </View>
   );
 };
--- src/PlayIcon.tsx
+++ src/PlayIcon.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Path, Svg } from 'react-native-svg';
+import { Image, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    height: 30,
     width: 30,
+    height: 30,
   },
 });
 
 export const PlayIcon: React.FC = () => {
   return (
-    <Svg style={styles.container} viewBox="0 0 384 512">
-      <Path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
-    </Svg>
+    <Image
+      resizeMode="contain"
+      style={styles.container}
+      source={require('./play.png')}
+    />
   );
 };
 
 export const PauseIcon: React.FC = () => {
   return (
-    <Svg style={styles.container} viewBox="0 0 320 512">
-      <Path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z" />
-    </Svg>
+    <Image
+      resizeMode="contain"
+      style={styles.container}
+      source={require('./pause.png')}
+    />
   );
 };
